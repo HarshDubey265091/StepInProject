f59a3e7e242c4757bb9aecd824e1de12df47ea79
Harsh <harshdubey2017@gmil.com>
2021-04-15 15:03:43 +0530

    commit1

---
+++ implementation/inc/fun.h
@@ -0,0 +1,8 @@
+#ifndef _FUN_H
+#define _FUN_H
+
+char *separatethehigher(char *s,int v);
+int matchcolour(char *str1,char *str2);
+int Search(char *Band_Colour);
+
+#endif
---
+++ implementation/src/separatecolour.c
@@ -0,0 +1,35 @@
+#include "fun.h"
+#include "conio.h"
+#include "string.h"
+#include "stdio.h"
+
+
+
+
+char *separatethehigher(char *s , int v)
+    {
+        char arr[3 * 12] = "";
+        char *p1 = arr;
+        size_t i = 0, len = 0;
+        
+        sprintf(arr,"%d",v);
+        
+        for(;*p1;p1++) {}
+		len = p1 - arr;
+            p1 = s + 3 * 12 - 2;
+            while(len--)
+            {
+                if(i++ == 3)
+                {
+                    i=1;
+                    *p1-- = ' ';
+                }
+                
+                *p1 = arr[len];
+                if(len) p1--;
+            }
+
+            for(i=0;*p1;p1++,i++) s[i] = *p1;
+            s[i] = *p1;
+            return s;   
+    }
