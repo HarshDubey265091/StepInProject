a043e30b26b50264ca4cff75567bf9beba223369
265091 <80531355+HarshDubey265091@users.noreply.github.com>
2021-04-15 15:35:36 +0530

    Add files via upload

---
+++ implementations/src/match.c
@@ -0,0 +1,17 @@
+#include "fun.h"
+#include "conio.h"
+#include "string.h"
+
+
+
+int matchcolour(char *str1,char *str2)
+    {
+        if(!str1 && !str2) return 0;
+        if(str1 && !str2) return 1;
+        if(!str1 && str2) return -1;
+
+        for(; *str1 && *str2 && *str1 == *str2; str1++,str2++){}
+
+        return *str1 - *str2;
+
+    }
